b1f74d02d2ba32e0be3e90b02226d900a9bd4eee
DESKTOP-SH1ABKH\user <kkwoncokr@gmail.com>
2021-01-02 20:58:12 +0900

    Working : userdetail

--- src/containers/user/UserDetailContainer.js
+++ src/containers/user/UserDetailContainer.js
@@ -6,8 +6,12 @@ import UserDetail from "../../components/user/UserDetail";
 const UserDetailContainer = ({ match }) => {
   const { user } = useSelector((state) => state.user);
   const { email } = match.params;
+  if (!user) {
+    return null;
+  }
   const findUser = user.find((v) => v.email === email);
-  return <UserDetail user={findUser} />;
+  console.log(findUser);
+  return <>{user ? <UserDetail user={findUser} /> : <div>준비중</div>}</>;
 };
 
 export default withRouter(UserDetailContainer);
