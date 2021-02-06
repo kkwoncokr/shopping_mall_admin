d5b33101debd5ba1213fa32d834a67d94c4b4dea
kkwon <kkwoncokr@gmail.com>
2021-02-06 15:20:14 +0900

    [Working] 상품관리 페이지 ing

--- src/components/product/ProductContent.js
+++ src/components/product/ProductContent.js
@@ -1,6 +1,50 @@
 import React from "react";
+import { Button, Card, Row, Col } from "antd";
+
+import styled from "styled-components";
 import DefalutLayout from "../ common/DefalutLayout";
 
-const ProductContent = () => <DefalutLayout>상품관리</DefalutLayout>;
-
+const ProductWrap = styled.article`
+  width: 100%;
+  padding: 20px 0;
+  box-sizing: border-box;
+`;
+const ProductContent = () => {
+  const { Meta } = Card;
+  const Cover = (
+    <img
+      alt="example"
+      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+    />
+  );
+  return (
+    <DefalutLayout>
+      <Button>추가히기</Button>
+      <ProductWrap>
+        <Row>
+          <Col span={6}>
+            <Card hoverable style={{ width: 240 }} cover={Cover}>
+              <Meta title="카드지갑" description="www.instagram.com" />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card hoverable style={{ width: 240 }} cover={Cover}>
+              <Meta title="카드지갑" description="www.instagram.com" />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card hoverable style={{ width: 240 }} cover={Cover}>
+              <Meta title="카드지갑" description="www.instagram.com" />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card hoverable style={{ width: 240 }} cover={Cover}>
+              <Meta title="카드지갑" description="www.instagram.com" />
+            </Card>
+          </Col>
+        </Row>
+      </ProductWrap>
+    </DefalutLayout>
+  );
+};
 export default React.memo(ProductContent);
